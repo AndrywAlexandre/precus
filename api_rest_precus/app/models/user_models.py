a2b2a5293aa818af3791e65061673e17d1885acd
andrywalexandre <andrywnatal@hotmail.com>
2025-04-10 00:01:51 -0300

    informando campos para referencia de models

--- api_rest_precus/app/models/user_models.py
+++ api_rest_precus/app/models/user_models.py
@@ -1,1 +1,12 @@
 # Modelos do SQLAlchemy (Banco de Dados)
+
+from sqlalchemy import Column, Integer, String
+from app.core.database import Base
+
+'''class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    email = Column(String, unique=True, index=True)
+    full_name = Column(String)'''
